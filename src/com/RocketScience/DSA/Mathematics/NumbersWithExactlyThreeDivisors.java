57a6751c9d4b621919d2ca74343eda56126085bb
Nirbhay Singh <nirbhay411@gmail.com>
2020-12-25 19:37:15 +0530

    Fix NumbersWithExactlyThreeDivisors Problem by Math.sqrt()

--- src/com/RocketScience/DSA/Mathematics/NumbersWithExactlyThreeDivisors.java
+++ src/com/RocketScience/DSA/Mathematics/NumbersWithExactlyThreeDivisors.java
@@ -2,13 +2,12 @@ package com.RocketScience.DSA.Mathematics;
 
 public class NumbersWithExactlyThreeDivisors {
     public static void main(String[] args) {
-        System.out.println(exactly3Divisors(67));
+        System.out.println(exactly3Divisors(999999));
     }
     public static int exactly3Divisors(int N)
     {
-        double x;
         int count = 0;
-        for(int i = 2;i<=N;i++){
+        for(int i = 2;i<=Math.sqrt(N);i++){
             if(PrimeCheck.isPrime(i)){
                 if(i*i <= N)
                     count++;
