eb32ba90a577df504b82dd7c5ceecf22fe91aad4
Nirbhay Singh <nirbhay411@gmail.com>
2021-01-16 16:32:21 +0530

    Improved the previous version: 1) Removed one extra for loop 2) Cleaned the code

--- src/com/RocketScience/DSA/Strings/Anagrams.java
+++ src/com/RocketScience/DSA/Strings/Anagrams.java
@@ -6,20 +6,21 @@ public class Anagrams {
         System.out.println(isAnagram(a,b));
     }
     private static boolean isAnagram(String a,String b){
-        int[] H = new int[26];
-        int i;
+        int[] H = new int[256];
+
         if(a.length() != b.length())
             return false;
-        else {
-            for (i = 0; i < a.length(); i++)
-                H[a.charAt(i) - 97]++;
-            for (i = 0; i < b.length(); i++) {
-                H[b.charAt(i) - 97]--;
-                if (H[b.charAt(i) - 97] < 0) {
-                    return false;
-                }
-            }
-            return i == b.length();
+
+        for(int i = 0;i<a.length();i++){
+            H[a.charAt(i)]++;
+            H[b.charAt(i)]--;
         }
+
+        for(int x : H){
+            if(x != 0)
+                return false;
+        }
+
+        return true;
     }
 }
