ab28c21f9335a7a13328fe0ae2ed1b623b723392
Nirbhay Singh <nirbhay411@gmail.com>
2021-01-16 16:38:20 +0530

    Added countSort Method

--- src/com/RocketScience/DSA/Sorting/CountingSort.java
+++ src/com/RocketScience/DSA/Sorting/CountingSort.java
@@ -12,7 +12,7 @@ public class CountingSort {
     public static void sort(int[] a){
         countingSort(a);
     }
-    private static void countingSort(int[] a){
+    public static void countingSort(int[] a){
         final int max = Arrays.stream(a).max().getAsInt();
         int[] count = new int[max+1];
         Arrays.fill(count,0);
@@ -25,5 +25,23 @@ public class CountingSort {
                 count[i]--;
             }else i++;
         }
+    }
+
+    //Time: O(n+k)  Space:O(n+k)
+    public static void countSort(int[] a, int n, int k){
+        int[] count=new int[k];
+        for(int i=0;i<n;i++)
+            count[a[i]]++;
+
+        for(int i=1;i<k;i++)
+            count[i]=count[i-1]+count[i];
+
+        int[] output=new int[n];
+        for(int i=n-1;i>=0;i--){
+            output[count[a[i]]-1]=a[i];
+            count[a[i]]--;
+        }
+        for(int i=0;i<n;i++)
+            a[i]=output[i];
     }
 }
