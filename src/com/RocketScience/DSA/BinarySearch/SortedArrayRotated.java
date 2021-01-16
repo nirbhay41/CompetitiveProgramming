d7563bdfbe63f1f548947bd01c3692ec66f4aca7
Nirbhay Singh <nirbhay411@gmail.com>
2021-01-16 16:48:26 +0530

    Now function returning index of min element instead of value

--- src/com/RocketScience/DSA/BinarySearch/SortedArrayRotated.java
+++ src/com/RocketScience/DSA/BinarySearch/SortedArrayRotated.java
@@ -2,21 +2,21 @@ package com.RocketScience.DSA.BinarySearch;
 
 public class SortedArrayRotated {
     public static void main(String[] args) {
-        System.out.println(minElement(new int[]{4,5,6,7,0,1,2}));
+        System.out.println(minElement(new int[]{2, 3, 4, 5, 6, 7, 8, 9, 10, 1}));
     }
     public static int minElement(int[] a){
         int N = a.length;
         if(a[0] < a[N-1])
             return 0;
-        int low = 0,high = N-1,mid,prev,next;
+        int low = 0,high = N-1,mid;
 
         while (low <= high){
-            if(a[low] <= a[high]) return a[low];
+            if(a[low] <= a[high]) return low;
             mid = low + (high - low)/2;
 
             if(a[mid] >= a[high]) low = mid+1;
             else high = mid;
         }
-        return a[low];
+        return -1;
     }
 }
