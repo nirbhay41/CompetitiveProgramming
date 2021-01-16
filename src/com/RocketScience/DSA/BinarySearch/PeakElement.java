dc3b870c1de95f459ff8afa49a7b2066d7758866
Nirbhay Singh <nirbhay411@gmail.com>
2021-01-16 16:46:08 +0530

    Re-wrote the program with more elegant code

--- src/com/RocketScience/DSA/BinarySearch/PeakElement.java
+++ src/com/RocketScience/DSA/BinarySearch/PeakElement.java
@@ -2,26 +2,20 @@ package com.RocketScience.DSA.BinarySearch;
 
 public class PeakElement {
     public static void main(String[] args) {
-        System.out.println(findPeak(new int[]{1,0,1,10,9,8,6,4,1}));
+        System.out.println(findPeak(new int[]{0,1,5,9,10,11,14,1}));
     }
     public static int findPeak(int[] a){
-        int low = 0,high = a.length-1,mid,prev,next;
+        int low = 0,high = a.length-1,mid;
 
-        while(low<high){
+        while(low<=high){
             mid = low + (high-low)/2;
 
-            if(mid == 0 && a[mid] > a[mid+1])
-                return a[mid];
-            else if(mid == a.length-1 && a[mid] > a[a.length-2])
-                return a[mid];
-            else if(mid > 0 && mid < a.length-1) {
-                if (a[mid - 1] <= a[mid] && a[mid] >= a[mid + 1])
-                    return a[mid];
-            }
+            if((mid == 0 || a[mid-1] <= a[mid]) && (mid == a.length-1 || a[mid] >= a[mid+1]))
+                return mid;
 
-            if(a[mid] <= a[mid+1])
-                 low = mid+1;
-            else high = mid-1;
+            if(mid > 0 && a[mid-1] >= a[mid])
+                high = mid-1;
+            else low = mid+1;
         }
         return -1;
     }
